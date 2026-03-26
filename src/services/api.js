503e91bb3c255b644a4e2e91d7763a646b0a80fa
Snow0796 <victorrodrigues.0796@gmail.com>
2026-03-26 16:01:34 -0300

    corrige URL da API para producao

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: 'https://saas-reviews-backend.onrender.com/api/v1',
 });
 
-// Adiciona o token automaticamente em todas as requisições
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
